2283ec333f56f89f30da9f69438cd30b7af77c18
yichen wang <785227920@qq.com>
2019-02-20 15:13:54 +0800

    贪吃蛇

--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,192 @@ namespace Snake
         {
             InitializeComponent();
         }
+        Snake snake = new Snake();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        Thread thread_SnakeHead = null;
+   
+        /// <summary>开始按钮(焦点在这里)</summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            thread_SnakeHead = new Thread(Init_Direction);
+            thread_SnakeHead.Start();
+
+
+        }
+  
+        /// <summary>用户控制移动方向(上下左右)</summary>
+        private void Move_keydown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+        /// <summary>初始化移动方向</summary>
+        public void Init_Direction()
+        {
+            int counter = 0;
+            Random random = new Random();
+            Snake.Direction = random.Next() % 4;
+            Console.WriteLine("初始方向为"+ Snake.Direction+","+"蛇头为"+Snake.List_Snake.Last());
+            while (snake.IsEND())
+            {
+                snake.Move(Snake.Direction);
+                ChangeColor(Snake.List_Snake.Last().ToString());
+                RestoreColor(Snake.List_Snake[0].ToString());
+                
+                //吃到食物
+                if (Snake.List_Snake.Last() == Snake.Food)
+                {
+                    //重新生成食物
+                    Snake.Food = snake.Init_Food();
+                    NewFood(Snake.Food.ToString());
+                    Console.WriteLine("吃到食物!");
+                }
+                else
+                {
+                    //要在还原尾巴颜色后删除
+                    Snake.List_Snake.RemoveAt(0);
+                }
+                Thread.Sleep(200);
+                counter += 1;
+                Console.WriteLine("移动" + counter + "," + "蛇头为" + Snake.List_Snake.Last() + "," +"蛇长"+Snake.List_Snake.Count()+","+"食物"+Snake.Food);
+            }
+            
+            MessageBox.Show(Snake.THE_END);
+            //SetText(id);
+            thread_SnakeHead.Abort();
+            
+        }
+
+
+        //委托
+        private delegate void SetDelegate(string value);
+        /// <summary>新蛇头变色</summary>
+        private void ChangeColor(string Text)
+        {
+            if (this.InvokeRequired)
+            {
+                SetDelegate d = new SetDelegate(ChangeColor);
+                this.Invoke(d, Text);
+            }
+            else
+            {
+                Button b = (Button)flowLayoutPanel1.Controls[Text];
+                b.BackColor = Color.Green;
+            }
+        }
+        /// <summary>尾巴还原颜色</summary>
+        private void RestoreColor(string Text)
+        {
+            if (this.InvokeRequired)
+            {
+                SetDelegate d = new SetDelegate(RestoreColor);
+                this.Invoke(d, Text);
+            }
+            else
+            {
+                Button b = (Button)flowLayoutPanel1.Controls[Text];
+                b.BackColor = Color.Gray;
+            }
+        }
+        /// <summary>新食物上色</summary>
+        private void NewFood(string Text)
+        {
+            if (this.InvokeRequired)
+            {
+                SetDelegate d = new SetDelegate(NewFood);
+                this.Invoke(d, Text);
+            }
+            else
+            {
+                Button b = (Button)flowLayoutPanel1.Controls[Text];
+                b.BackColor = Color.Yellow;
+
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            snake.Init_Snake();
+            Snake.Food = snake.Init_Food();
+            //Snake.Direction = 5;
+            this.flowLayoutPanel1.Controls.Clear();
+            for (int i = 0; i < 400; i++)
+            {
+                Button b = new Button();
+                //边界
+                if (i < 21 || i>378 || i %20 ==0 ||(i+1) % 20 ==0)
+                {
+                    b.BackColor = Color.Red;
+                }
+                //区域
+                else
+                {
+                    b.BackColor = Color.Gray;
+                }
+
+                b.Width = 25;
+                b.Height = 25;
+                //b1.Text = i.ToString();
+                b.Name = i.ToString();
+                //b.Text = i.ToString();
+                b.Enabled = false;
+                b.Margin = new Padding(0, 0, 0, 0);
+
+                //每5格,就往下移动
+                //b1.Top = b1.Width * (i / 5);
+                //每5次重置
+                //b1.Left = b1.Height * (i % 5);
+                b.FlatAppearance.BorderSize = 0;
+                b.FlatStyle = FlatStyle.Flat;
+
+
+                this.flowLayoutPanel1.Controls.Add(b);
+            }
+            Button b1 = (Button)flowLayoutPanel1.Controls[Snake.Food.ToString()];
+            b1.BackColor = Color.Yellow;
+            Button b2 = (Button)flowLayoutPanel1.Controls[Snake.List_Snake.Last().ToString()];
+            b2.BackColor = Color.Green;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+            {
+                if (!(Snake.Direction == 0 || Snake.Direction == 1))
+
+                    Snake.Direction = 0;
+            }
+
+            if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                if (!(Snake.Direction == 0 || Snake.Direction == 1))
+                    Snake.Direction = 1;
+            }
+
+            if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
+            {
+                if (!(Snake.Direction == 2 || Snake.Direction == 3))
+                    Snake.Direction = 2;
+            }
+
+            if (e.KeyCode == Keys.D | e.KeyCode == Keys.Right)
+            {
+                if (!(Snake.Direction == 2 || Snake.Direction == 3))
+                    Snake.Direction = 3;
+            }
+        }
     }
-}
+        }
+    
+
---
+++ Snake/Snake.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    class Snake
+    {
+        /// <summary>蛇身的集合,最后一个元素为蛇头</summary>
+        public static List<int> List_Snake = new List<int>();
+        /// <summary>食物(食物的集合,根据蛇身动态变化)</summary>
+        public static List<int> List_Food = new List<int>();
+        /// <summary>初始化蛇头</summary>
+        public static int SnakeHead;
+        /// <summary>初始化食物</summary>
+        public static int Food;
+        /// <summary>结束语</summary>
+        public static string THE_END ;
+        /// <summary>移动方向(0123为上下左右)</summary>
+        public static int Direction ;
+        /// <summary>是否结束(false为结束)</summary>
+        public bool IsEND()
+        {
+            //是否咬到自己
+            for (int i = 0; i < List_Snake.Count() - 1; i++)
+            {
+                if (List_Snake.Last() == List_Snake[i])
+                {
+                    Console.WriteLine("咬自己");
+                    THE_END = "得分:" + List_Snake.Count.ToString() + "\r\n咬到自己了!!蠢货";
+                    return false;
+                }
+
+            }
+            //结束
+            if (List_Snake.Last() < 21 || List_Snake.Last() % 20 == 0 || (List_Snake.Last() + 1) % 20 == 0 || 399 - List_Snake.Last() < 20)
+            {
+                Console.WriteLine("撞墙");
+                THE_END += "得分:" + List_Snake.Count.ToString() + "\r\n撞墙了!!蠢货";
+                return false;
+            }
+            //食物集合大小为0
+            if (List_Food.Count == 0)
+            {
+                THE_END += "得分:" + List_Snake.Count.ToString() + "\r\n恭喜你通关了!!!";
+                return false;
+            }
+                return true;
+
+            
+        }
+        /// <summary>移动,更新list蛇头</summary>
+        public void Move(int direction)
+        {
+            switch (direction)
+            {
+                case 0:
+                    List_Snake.Add((List_Snake.Last() - 20));
+                    break;
+                case 1:
+                    List_Snake.Add((List_Snake.Last()) + 20);
+                    break;
+                case 2:
+                    List_Snake.Add(List_Snake.Last() - 1);
+                    break;
+                case 3:
+                    List_Snake.Add(List_Snake.Last() + 1);
+                    break;
+
+            }
+           
+            
+        }
+
+        /// <summary>初始化蛇身(list)</summary>
+        public List<int> Init_Snake()
+        {
+            List_Snake.Clear();
+            Random random = new Random();
+            List_Snake.Add(random.Next() % 200);
+            while (true)
+            {
+                //如果为边界,重新随机
+                if (List_Snake[0] < 21 || List_Snake[0] > 378 || List_Snake[0] % 20 == 0 || (List_Snake[0] + 1) % 20 == 0)
+                {
+                    List_Snake[0] = random.Next() % 200;
+                }
+                else
+                    return List_Snake;
+            }
+            
+        }
+        /// <summary>初始化食物</summary>
+        public int Init_Food()
+        {
+            List_Food.Clear();
+            for (int i = 0; i < 400; i++)
+            {
+                if (!(i < 21 || i > 378 || i % 20 == 0 || (i + 1) % 20 == 0))
+                {
+                    List_Food.Add(i);
+                }
+            }
+             List_Food = List_Food.Except(List_Snake).ToList() ;
+            Random random = new Random();
+
+            return List_Food[ random.Next() % (List_Food.Count)];
+
+        }
+
+    }
+
+
+}
